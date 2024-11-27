b1e9011c9da20a921bf1fe465c7aab8494318f12
Leonel Quintana <leonelquint@gmail.com>
2024-11-27 11:33:53 -0300

    git commit -m "ruta img"

--- src/data/productData.js
+++ src/data/productData.js
@@ -4,28 +4,28 @@ const products = [
       name: 'Diseño Web',
       category: '1',
       description: 'Ofrecemos diseño web profesional adaptado a tus necesidades, con interfaces modernas y fáciles de usar.Creamos sitios rápidos, seguros y responsivos para optimizar la experiencia del usuario.Aumenta tu presencia en línea y mejora tus ventas con un diseño único y funcional.',
-      imageUrl: "assets/img/Serv1.jpg"
+      imageUrl: "/assets/img/Serv1.jpg"
     },
     {
       id: 2,
       name: 'Gestión de Redes Sociales',
       category: '2',
       description: '¡Transforma tu presencia digital! 🚀 Te ayudamos a crear contenido atractivo y estrategias efectivas para conectar con tu audiencia en redes sociales. 📱 Aumenta tu visibilidad, genera interacción y consigue más clientes. ¡Es momento de llevar tu marca al siguiente nivel! #MarketingDigital #RedesSociales #CrecimientoOnline',
-      imageUrl: "assets/img/Serv2.jpg"
+      imageUrl: "/assets/img/Serv2.jpg"
     },
     {
       id: 3,
       name: 'Branding',
       category: '1',
       description: 'Construye una identidad de marca sólida y memorable. 💡 Te ayudamos a definir tu estilo, valores y mensaje único para conectar de forma auténtica con tu público. ¡Haz que tu marca se distinga y se quede en la mente de tus clientes! #Branding #IdentidadDeMarca #EstrategiaDeMarca',
-      imageUrl: "assets/img/Serv3.jpg"
+      imageUrl: "/assets/img/Serv3.jpg"
     },
     {
       id: 4,
       name: 'Creación de Contenido',
       category: '2',
       description: 'Impulsa tu marca con contenido creativo y relevante que capture la atención de tu audiencia. 🎯 Creamos publicaciones, videos y gráficos personalizados para contar tu historia de manera impactante. ¡Haz que tu marca se destaque y conecta con tus seguidores de manera auténtica! #CreacionDeContenido #MarketingDigital #ContenidoDeValor',
-      imageUrl: "assets/img/Serv4.jpg"
+      imageUrl: "/assets/img/Serv3.jpg"
     }
   ];
   
